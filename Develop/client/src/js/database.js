d3b922574a8a8e263e733d110cab35b5167a32e3
Phillip Lam <philliplamm07@gmail.com>
2024-10-21 17:05:34 +1100

    Jate editor

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -12,10 +12,29 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// TODO: Add logic to a method that accepts some content and adds it to the database (DONE)
+export const putDb = async (content) => {
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ value: content, id: 1 });
+  const result = await request;
+  console.log('Text saved to the database', result);
+}
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// console.error('putDb not implemented');
+
+// TODO: Add logic for a method that gets all the content from the database (DONE)
+export const getDb = async () => {
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.get(1);
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+}
+  
+// console.error('getDb not implemented');
 
 initdb();
